b03fc011f4ca2fc43feaada4062bceb87f263833
Chris Malley <cmalley@pixelzoom.com>
2013-08-13 15:56:09 -0600

    move static methods out of prototype, pull G out function so it's not allocated everytime the function is called.

--- js/model/GravityForceLabModel.js
+++ js/model/GravityForceLabModel.js
@@ -12,6 +12,16 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Range = require( 'DOT/Range' );
 
+  var G = 6.67384E-11; // gravitational constant
+
+  var calculateForce = function( mass1, mass2, distance ) {
+    return ( G * mass1 * mass2 ) / ( distance * distance );
+  };
+
+  var calculateDistance = function( x1, x2 ) {
+    return Math.abs( Math.round( ( x2 - x1 ) / 50 * 100 ) / 100 );
+  };
+
   function GravityForceLabModel( width, height ) {
 
     var model = this;
@@ -25,15 +35,15 @@ define( function( require ) {
       mass1: 38.00,
       mass2: 25.00,
       showValues: true,
-      distance: 4,
+      distance: 4, // separation between the 2 masses
       force: 0,
       locationX1: this.width / 2 - 100, // x-coordinate of mass 1
       locationX2: this.width / 2 + 100, // x-coordinate of mass 2
       ruler: {x: 120, y: 310}
     } );
 
-    var updateForce = function() {model.force = model.gravityForce( model.mass1, model.mass2, model.distance );};
-    var updateDistance = function() {model.distance = model.calculateDistance( model.locationX1, model.locationX2 );};
+    var updateForce = function() {model.force = calculateForce( model.mass1, model.mass2, model.distance );};
+    var updateDistance = function() {model.distance = calculateDistance( model.locationX1, model.locationX2 );};
 
     this.mass1Property.link( updateForce );
     this.mass2Property.link( updateForce );
@@ -50,24 +60,15 @@ define( function( require ) {
       this.trigger( 'mass2Step' );
     },
     reset: function() {
-
       this.mass1Property.reset();
       this.mass2Property.reset();
       this.locationX1Property.reset();
       this.locationX2Property.reset();
       this.showValuesProperty.reset();
       this.distanceProperty.reset();
-      this.force = this.gravityForce( this.mass1, this.mass2, this.distance );
+      this.force = calculateForce( this.mass1, this.mass2, this.distance );
       this.rulerProperty.reset();
-    },
-    gravityForce: function( mass1, mass2, distance ) {
-      var G = 6.67384E-11;
-      return ( G * mass1 * mass2 ) / ( distance * distance );
-    },
-    calculateDistance: function( x1, x2 ) {
-      return Math.abs( Math.round( ( x2 - x1 ) / 50 * 100 ) / 100 );
     }
-
   } );
 
   return GravityForceLabModel;
