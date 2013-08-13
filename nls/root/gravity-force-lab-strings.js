cac75d175b43180641143cfdca69bfdcc5dc50b1
Chris Malley <cmalley@pixelzoom.com>
2013-08-13 09:52:15 -0600

    move English strings to nls/root/gravity-force-lab-strings.js, closes #19

---
+++ nls/root/gravity-force-lab-strings.js
@@ -0,0 +1,15 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+define( {
+  "GFL.name": "Gravity Force Lab",
+  "GFL.mass1": "Mass 1",
+  "GFL.mass2": "Mass 2",
+  "GFL.showValues": "Show values",
+  "GFL.unitKg": "kg",
+  "GFL.unitMeters": "meters",
+  "GFL.title1": "Force on m1 by m2",
+  "GFL.title2": "Force on m2 by m1",
+  "GFL.unitN": "N",
+  "GFL.label1": "m1",
+  "GFL.label2": "m2"
+} );
