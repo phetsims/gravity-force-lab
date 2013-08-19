5298d712d8e9284d1e5d02210dcc7214790315fb
Vasily Shakhov <vasily.shakhov@mlearner.com>
2013-08-19 17:25:40 +0400

    fixed #21

--- js/view/PullObject.js
+++ js/view/PullObject.js
@@ -16,33 +16,36 @@ define( function( require ) {
   var Image = require( 'SCENERY/nodes/Image' );
   var gravityForceLabImages = require( 'gravity-force-lab-images' );
 
+  var IMAGES_COUNT = 15;
+  var ROPE_LENGTH = 50;
 
   function PullObject() {
 
     Node.call( this );
 
-    var pullGroup = new Node( {x: -50} ); //TODO #27: 50 appears 3 times, should be factored out
-    var pull = [];
-    var i;
-    for ( i = 0; i < 15; i++ ) {
-      pull.push( new Image( gravityForceLabImages.getImage( "pull_figure_" + i + ".png" ) ) );
+    var pullGroup = new Node( {x: -ROPE_LENGTH} ),
+      pull = [],
+      i;
+    for ( i = 0; i < IMAGES_COUNT; i++ ) {
+      var image = new Image( gravityForceLabImages.getImage( "pull_figure_" + i + ".png" ) );
+      pull.push(new Node({children:[new Path( { shape: Shape.circle( 0, 0, 10 ), fill: '#777', pickable: false, scale:{x:image.width/20,y:1},x:image.width/2,y:image.height-5 } ),image]})  );
     }
-
-    for ( i = 0; i < 15; i++ ) {  //TODO #27: 15 appears twice, should be factored out
+    pullGroup.addChild( new Path( { shape: Shape.lineSegment( -ROPE_LENGTH, 0, 0, 0 ), stroke: '#666', lineWidth: 2, pickable: false } ) );
+    for ( i = 0; i < IMAGES_COUNT; i++ ) {
       pullGroup.addChild( pull[i] );
       pull[i].scale( -0.3, 0.3 );
-      pull[i].bottom = 31;
-      pull[i].right = -5 + i;
+      pull[i].bottom = 33;
+      pull[i].right = i - ROPE_LENGTH;
       pull[i].setVisible( false );
     }
-    pullGroup.addChild( new Path( { shape: Shape.lineSegment( -5, 0, 50, 0 ), stroke: '#666', lineWidth: 2, pickable: false } ) );
+
     this.addChild( pullGroup );
     //function select image
     this.setPull = function( value, offsetX ) {
-      for ( var i = 0; i < 15; i++ ) {
+      for ( var i = 0; i < IMAGES_COUNT; i++ ) {
         pull[i].setVisible( i === value );
       }
-      pullGroup.x = -50 - offsetX;
+      pullGroup.x = -offsetX;
     };
   }
 
