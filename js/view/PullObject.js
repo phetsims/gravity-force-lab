6812097f9b17ebaba6916953857f68656ebe4776
samreid <reids@colorado.edu>
2013-10-08 23:26:57 -0600

    simplified image and audio paths, see https://github.com/phetsims/chipper/issues/37

--- js/view/PullObject.js
+++ js/view/PullObject.js
@@ -14,21 +14,21 @@ define( function( require ) {
   var Path = require( 'SCENERY/nodes/Path' );
   var Shape = require( 'KITE/Shape' );
   var Image = require( 'SCENERY/nodes/Image' );
-  var pullImage0 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_0.png' );
-  var pullImage1 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_1.png' );
-  var pullImage2 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_2.png' );
-  var pullImage3 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_3.png' );
-  var pullImage4 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_4.png' );
-  var pullImage5 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_5.png' );
-  var pullImage6 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_6.png' );
-  var pullImage7 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_7.png' );
-  var pullImage8 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_8.png' );
-  var pullImage9 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_9.png' );
-  var pullImage10 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_10.png' );
-  var pullImage11 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_11.png' );
-  var pullImage12 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_12.png' );
-  var pullImage13 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_13.png' );
-  var pullImage14 = require( 'image!GRAVITY_FORCE_LAB/../images/pull_figure_14.png' );
+  var pullImage0 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_0.png' );
+  var pullImage1 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_1.png' );
+  var pullImage2 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_2.png' );
+  var pullImage3 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_3.png' );
+  var pullImage4 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_4.png' );
+  var pullImage5 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_5.png' );
+  var pullImage6 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_6.png' );
+  var pullImage7 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_7.png' );
+  var pullImage8 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_8.png' );
+  var pullImage9 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_9.png' );
+  var pullImage10 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_10.png' );
+  var pullImage11 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_11.png' );
+  var pullImage12 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_12.png' );
+  var pullImage13 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_13.png' );
+  var pullImage14 = require( 'image!GRAVITY_FORCE_LAB/pull_figure_14.png' );
 
   var pullImages = [pullImage0, pullImage1, pullImage2, pullImage3, pullImage4, pullImage5, pullImage6, pullImage7, pullImage8, pullImage9, pullImage10,
     pullImage11, pullImage12, pullImage13, pullImage14];
