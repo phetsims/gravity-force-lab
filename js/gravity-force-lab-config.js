f4b2edbe6657bd0db2854cfa7fed2bd3a8f3f5b4
Sam Reid <reids@colorado.edu>
2015-04-23 11:43:40 -0600

    Point to text-2.0.12 and deleted text.js, see https://github.com/phetsims/sherpa/issues/15

--- js/gravity-force-lab-config.js
+++ js/gravity-force-lab-config.js
@@ -13,7 +13,7 @@ require.config( {
   paths: {
 
     // third party libs
-    text: '../../sherpa/text',
+    text: '../../sherpa/text-2.0.12',
 
     // plugins
     audio: '../../chipper/js/requirejs-plugins/audio',
