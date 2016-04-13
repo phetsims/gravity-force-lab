87a191ad675f7759d4253fb308f1d0dcaf8e1c2d
samreid <reids@colorado.edu>
2016-04-13 13:30:57 -0600

    Moved TandemDragHandler to tandem/ and added tandem to config, see https://github.com/phetsims/tandem/issues/9

--- js/gravity-force-lab-config.js
+++ js/gravity-force-lab-config.js
@@ -33,6 +33,7 @@ require.config( {
     SCENERY: '../../scenery/js',
     SCENERY_PHET: '../../scenery-phet/js',
     SUN: '../../sun/js',
+    TANDEM: '../../tandem/js',
 
     // this sim
     GRAVITY_FORCE_LAB: '.'
