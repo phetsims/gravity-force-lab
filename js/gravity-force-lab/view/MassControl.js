d367e471a0fee4a28de52cdf1c14854be5b17a1c
samreid <reids@colorado.edu>
2016-11-14 09:44:16 -0700

    Converted MassControl to use NumberControl, see https://github.com/phetsims/gravity-force-lab/issues/81

--- js/gravity-force-lab/view/MassControl.js
+++ js/gravity-force-lab/view/MassControl.js
@@ -10,17 +10,14 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var ArrowButton = require( 'SUN/buttons/ArrowButton' );
   var Dimension2 = require( 'DOT/Dimension2' );
   var gravityForceLab = require( 'GRAVITY_FORCE_LAB/gravityForceLab' );
-  var HSlider = require( 'SUN/HSlider' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
   var Panel = require( 'SUN/Panel' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
-  var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
   var Text = require( 'SCENERY/nodes/Text' );
+  var NumberControl = require( 'SCENERY_PHET/NumberControl' );
+  var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
 
   // strings
   var pattern0Value1UnitsString = require( 'string!GRAVITY_FORCE_LAB/pattern_0value_1units' );
@@ -35,12 +32,37 @@ define( function( require ) {
    * @param {Property.<number>} massProperty
    * @param {Range} massRange
    * @param {Color} thumbColor
-   * @param {Object} [options]
    * @constructor
    */
-  function MassControl( titleString, massProperty, massRange, thumbColor, options ) {
+  function MassControl( titleString, massProperty, massRange, thumbColor ) {
 
-    options = _.extend( {
+    // major ticks
+    var tickLabelOptions = { font: new PhetFont( 14 ), pickable: false };
+
+    var numberControl = new NumberControl( titleString, massProperty, massRange, {
+      titleFont: new PhetFont( 24 ),
+      valueFont: new PhetFont( 20 ),
+
+      // Don't fill in the {0}, it will be filled in by NumberControl
+      valuePattern: StringUtils.format( pattern0Value1UnitsString, '{0}', unitsKgString ),
+      majorTicks: [ {
+        value: massRange.min,
+        label: new Text( massRange.min, tickLabelOptions )
+      }, {
+        value: massRange.max,
+        label: new Text( massRange.max, tickLabelOptions )
+      } ],
+      layoutFunction: NumberControl.createLayoutFunction3(),
+      thumbFillEnabled: thumbColor.colorUtilsBrighter( 0.15 ),
+      thumbFillHighlighted: thumbColor,
+      arrowButtonScale: 1,
+      trackSize: TRACK_SIZE,
+      trackFillEnabled: 'black',
+      thumbSize: THUMB_SIZE,
+      majorTickLength: ( THUMB_SIZE.height / 2 ) + ( TRACK_SIZE.height / 2 ) + 2
+    } );
+
+    Panel.call( this, numberControl, {
       fill: '#FDF498',
       xMargin: 15,
       yMargin: 10,
@@ -48,77 +70,6 @@ define( function( require ) {
       minWidth: 224,
       resize: false,
       align: 'right'
-    }, options );
-
-    var slider = new HSlider( massProperty, massRange, {
-      trackSize: TRACK_SIZE,
-      trackFillEnabled: 'black',
-      thumbSize: THUMB_SIZE,
-      majorTickLength: ( THUMB_SIZE.height / 2 ) + ( TRACK_SIZE.height / 2 ) + 2,
-      thumbFillEnabled: thumbColor.colorUtilsBrighter( 0.15 ),
-      thumbFillHighlighted: thumbColor
-    } );
-
-    // major ticks
-    var tickLabelOptions = { font: new PhetFont( 14 ), pickable: false };
-    slider.addMajorTick( massRange.min, new Text( massRange.min, tickLabelOptions ) );
-    slider.addMajorTick( massRange.max, new Text( massRange.max, tickLabelOptions ) );
-
-    var plusButton = new ArrowButton( 'right', function propertyPlus() {
-      massProperty.set( Math.min( massProperty.get() + 1, massRange.max ) );
-    } );
-
-    var minusButton = new ArrowButton( 'left', function propertyMinus() {
-      massProperty.set( Math.max( massProperty.get() - 1, massRange.min ) );
-    } );
-
-    var valueField = new Rectangle( 0, 0, 100, 30, 3, 3, {
-      fill: '#FFF',
-      stroke: 'black',
-      lineWidth: 1,
-      pickable: false
-    } );
-
-    var valueLabel = new Text( '', { font: new PhetFont( 18 ), pickable: false, maxWidth: valueField.width * 0.9 } );
-
-    var title = new Text( titleString, {
-      font: new PhetFont( 24 ),
-      pickable: false,
-      maxWidth: slider.width
-    } );
-
-    // rendering order
-    var content = new Node( {
-      children: [
-        valueField,
-        valueLabel,
-        title,
-        slider,
-        plusButton,
-        minusButton
-      ]
-    } );
-
-    // relative layout, everything relative to the track
-    valueField.centerX = slider.centerX;
-    valueField.bottom = slider.top - 10; // value centered above the track
-    valueLabel.centerX = valueField.centerX;
-    valueLabel.centerY = valueField.centerY;
-    title.centerX = valueField.centerX;
-    title.bottom = valueField.top - 6; // title centered above the value
-    plusButton.left = valueField.right + 10; // plus button to the right of the value
-    plusButton.centerY = valueField.centerY;
-    minusButton.right = valueField.left - 10; // minus button to the left of the value
-    minusButton.centerY = valueField.centerY;
-    slider.centerX = slider.centerX + 5; // empirically determined for positioning
-
-    Panel.call( this, content, options );
-
-    massProperty.link( function updateMass( value ) {
-      valueLabel.text = StringUtils.format( pattern0Value1UnitsString, Math.floor( value ), unitsKgString );
-      valueLabel.centerX = valueField.centerX; // keep the value centered in the field
-      plusButton.enabled = ( Math.floor( value ) < massRange.max );
-      minusButton.enabled = ( Math.floor( value ) > massRange.min );
     } );
   }
 
