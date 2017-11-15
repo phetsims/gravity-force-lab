f4a737cd59a48fd15a1b1fd56111c5bf68c06910
samreid <reids@colorado.edu>
2017-11-15 16:44:16 -0700

    Renamed TType=>TypeIO, see https://github.com/phetsims/phet-io/issues/1265

--- js/gravity-force-lab/model/GravityForceLabModel.js
+++ js/gravity-force-lab/model/GravityForceLabModel.js
@@ -21,7 +21,7 @@ define( function( require ) {
   var Property = require( 'AXON/Property' );
   var Range = require( 'DOT/Range' );
   var PropertyIO = require( 'AXON/PropertyIO' );
-  var TVector2 = require( 'DOT/TVector2' );
+  var Vector2IO = require( 'DOT/Vector2IO' );
   var Vector2 = require( 'DOT/Vector2' );
 
   // phet-io modules
@@ -35,7 +35,7 @@ define( function( require ) {
 
     this.rulerPositionProperty = new Property( new Vector2( 0, -0.90 ), {
       tandem: tandem.createTandem( 'rulerPositionProperty' ),
-      phetioType: PropertyIO( TVector2 )
+      phetioType: PropertyIO( Vector2IO )
     } ); // @public
 
     this.constantRadiusProperty = new Property( false, {
