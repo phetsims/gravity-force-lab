2280901c8aebc975268fc83c4a763d6063304b11
samreid <reids@colorado.edu>
2018-01-07 08:19:42 -0700

    Updated copyright dates, see https://github.com/phetsims/chipper/issues/643

--- js/gravity-force-lab/view/MassControl.js
+++ js/gravity-force-lab/view/MassControl.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2017, University of Colorado Boulder
+// Copyright 2013-2018, University of Colorado Boulder
 
 /**
  * Arrow buttons, slider and text box for editing the mass amount.
