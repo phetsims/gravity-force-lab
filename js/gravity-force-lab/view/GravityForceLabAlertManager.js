169c34682480aa8d23bf80886c5318e29238aed2
Michael Barlow <m.barlow12@gmail.com>
2019-01-09 18:18:22 -0700

    comment out common position alert functions, moved to ISLCAlertManager

--- js/gravity-force-lab/view/GravityForceLabAlertManager.js
+++ js/gravity-force-lab/view/GravityForceLabAlertManager.js
@@ -6,10 +6,10 @@ define( require => {
   // modules
   const gravityForceLab = require( 'GRAVITY_FORCE_LAB/gravityForceLab' );
   const GravityForceLabA11yStrings = require( 'GRAVITY_FORCE_LAB/gravity-force-lab/GravityForceLabA11yStrings' );
-  const GravityForceLabForceDescriber = require( 'GRAVITY_FORCE_LAB/gravity-force-lab/view/describers/GravityForceLabForceDescriber' );
+  // const GravityForceLabForceDescriber = require( 'GRAVITY_FORCE_LAB/gravity-force-lab/view/describers/GravityForceLabForceDescriber' );
   const GravityForceLabPositionDescriber = require( 'GRAVITY_FORCE_LAB/gravity-force-lab/view/describers/GravityForceLabPositionDescriber' );
   const ISLCObjectEnum = require( 'INVERSE_SQUARE_LAW_COMMON/view/ISLCObjectEnum' );
-  const ISLCA11yStrings = require( 'INVERSE_SQUARE_LAW_COMMON/ISLCA11yStrings' );
+  // const ISLCA11yStrings = require( 'INVERSE_SQUARE_LAW_COMMON/ISLCA11yStrings' );
   const ISLCAlertManager = require( 'INVERSE_SQUARE_LAW_COMMON/view/ISLCAlertManager' );
   const MassDescriber = require( 'GRAVITY_FORCE_LAB/gravity-force-lab/view/describers/MassDescriber' );
   const StringUtils = require( 'PHETCOMMON/util/StringUtils' );
@@ -24,7 +24,7 @@ define( require => {
   const constantRadiusThinkDensityString = GravityForceLabA11yStrings.constantRadiusThinkDensity.value;
   const massAndForceClausesPatternString = GravityForceLabA11yStrings.massAndForceClausesPattern.value;
   const massSizeRelativeSizePatternString = GravityForceLabA11yStrings.massSizeRelativeSizePattern.value;
-  const regionForceClausePatternString = ISLCA11yStrings.regionForceClausePattern.value;
+  // const regionForceClausePatternString = ISLCA11yStrings.regionForceClausePattern.value;
 
   class GravityForceLabAlertManager extends ISLCAlertManager {
     constructor( model ) {
@@ -33,7 +33,7 @@ define( require => {
 
       // a11y describers - initialized outside the class declaration as they should be treated like helper functions
       this.massDescriber = MassDescriber.getDescriber();
-      this.forceDescriber = GravityForceLabForceDescriber.getDescriber();
+      // this.forceDescriber = GravityForceLabForceDescriber.getDescriber();
 
       model.scientificNotationProperty.lazyLink( displayInScientificNotation => {
         this.alertScientificNotation( displayInScientificNotation );
@@ -95,17 +95,17 @@ define( require => {
       utteranceQueue.addToBack( utterance );
     }
 
-    alertPositionChanged( endAtEdge ) {
-      const alert = this.getPositionChangedAlertText( endAtEdge );
-      const utterance = new Utterance( { alert, uniqueGroupId: 'position' } );
-      utteranceQueue.addToBack( utterance );
-    }
-
-    alertPositionUnchanged() {
-      const alert = this.getPositionUnchangedAlertText();
-      const utterance = new Utterance( { alert, uniqueGroupId: 'position' } );
-      utteranceQueue.addToBack( utterance );
-    }
+    // alertPositionChanged( endAtEdge ) {
+    //   const alert = this.getPositionChangedAlertText( endAtEdge );
+    //   const utterance = new Utterance( { alert, uniqueGroupId: 'position' } );
+    //   utteranceQueue.addToBack( utterance );
+    // }
+    //
+    // alertPositionUnchanged() {
+    //   const alert = this.getPositionUnchangedAlertText();
+    //   const utterance = new Utterance( { alert, uniqueGroupId: 'position' } );
+    //   utteranceQueue.addToBack( utterance );
+    // }
 
     /**
      * Returns the filled-in string, '{{massValue}} kilograms, {{size}}, {{relativeSize}} {{otherObjectLabel}}'
@@ -140,24 +140,24 @@ define( require => {
       return StringUtils.fillIn( massAndForceClausesPatternString, { massClause, forceClause } );
     }
 
-    getPositionChangedAlertText( endAtEdge ) {
-      let alertText = this.forceDescriber.getVectorChangeText();
-      let edgeAlertText = this.forceDescriber.getVectorSizeText();
-
-      if ( this.model.forceValuesProperty.get() ) {
-        alertText = this.forceDescriber.getVectorChangeForcesNowText();
-        edgeAlertText = this.forceDescriber.getVectorSizeForceValueText();
-      }
-
-      return endAtEdge ? edgeAlertText : alertText;
-    }
-
-    getPositionUnchangedAlertText() {
-      const positionDescriber = GravityForceLabPositionDescriber.getDescriber();
-      const forceClause = this.forceDescriber.getVectorsAndForcesClause();
-      const region = positionDescriber.qualitativeDistance;
-      return StringUtils.fillIn( regionForceClausePatternString, { region, forceClause } );
-    }
+    // getPositionChangedAlertText( endAtEdge ) {
+    //   let alertText = this.forceDescriber.getVectorChangeText();
+    //   let edgeAlertText = this.forceDescriber.getVectorSizeText();
+    //
+    //   if ( this.model.forceValuesProperty.get() ) {
+    //     alertText = this.forceDescriber.getVectorChangeForcesNowText();
+    //     edgeAlertText = this.forceDescriber.getVectorSizeForceValueText();
+    //   }
+    //
+    //   return endAtEdge ? edgeAlertText : alertText;
+    // }
+    //
+    // getPositionUnchangedAlertText() {
+    //   const positionDescriber = GravityForceLabPositionDescriber.getDescriber();
+    //   const forceClause = this.forceDescriber.getVectorsAndForcesClause();
+    //   const region = positionDescriber.qualitativeDistance;
+    //   return StringUtils.fillIn( regionForceClausePatternString, { region, forceClause } );
+    // }
 
     static getManager() {
       return ISLCAlertManager.getManager();
