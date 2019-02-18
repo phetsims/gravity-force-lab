7b2bc1edd2df8d90179d8a5303eaac0a85edaac2
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-18 14:21:43 -0700

    Update copyright year, see https://github.com/phetsims/tasks/issues/988

--- js/gravity-force-lab/view/GravityForceLabScreenSummaryNode.js
+++ js/gravity-force-lab/view/GravityForceLabScreenSummaryNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
  * A node to control the PDOM content for the Screen Summary of the sim.
