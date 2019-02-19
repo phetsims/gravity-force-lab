e81c4fb531a1b9bc0c01796448a6e742c04aeb99
zepumph <michael.kauzmann@colorado.edu>
2019-02-19 13:30:36 -0900

    add farthest and closest qualitative distance strings, see https://github.com/phetsims/gravity-force-lab-basics/issues/88

--- js/gravity-force-lab/view/describers/GravityForceLabPositionDescriber.js
+++ js/gravity-force-lab/view/describers/GravityForceLabPositionDescriber.js
@@ -56,28 +56,34 @@ define( require => {
     getDistanceIndex( distance ) {
       assert && assert( distance > 0, 'Distance between spheres should always be positive.' );
 
-      if ( distance >= 8.2 ) {
+      if ( distance === 9.6 ) {
         return 0;
       }
-      if ( distance >= 6.7 ) {
+      if ( distance >= 8.5 ) {
         return 1;
       }
-      if ( distance >= 5.2 ) {
+      if ( distance >= 6.9 ) {
         return 2;
       }
-      if ( distance >= 3.7 ) {
+      if ( distance >= 5.3 ) {
         return 3;
       }
-      if ( distance >= 2.2 ) {
+      if ( distance >= 3.7 ) {
         return 4;
       }
-      if ( distance >= 0.9 ) {
+      if ( distance >= 2.2 ) {
         return 5;
       }
-      if ( distance < 0.9 ) {
+      if ( distance >= 1.0 ) {
         return 6;
       }
-      throw new Error( 'Invalid distance value' );
+      if ( distance >= 0.4 ) {
+        return 7;
+      }
+      if ( distance === 0.3 ) {
+        return 8;
+      }
+      assert && assert( false, 'Invalid distance value' );
     }
 
     /**
