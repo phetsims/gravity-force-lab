236ed8844f000e605066f430df9e0a3021a5335e
Jesse Greenberg <jesse.greenberg@colorado.edu>
2019-04-06 17:55:45 -0600

    convert to es6

--- js/gravity-force-lab/view/GravityForceLabKeyboardHelpContent.js
+++ js/gravity-force-lab/view/GravityForceLabKeyboardHelpContent.js
@@ -6,153 +6,149 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  * @author Michael Barlow
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var ArrowKeyNode = require( 'SCENERY_PHET/keyboard/ArrowKeyNode' );
-  var EndKeyNode = require( 'SCENERY_PHET/keyboard/EndKeyNode' );
-  var GeneralKeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/GeneralKeyboardHelpSection' );
-  var gravityForceLab = require( 'GRAVITY_FORCE_LAB/gravityForceLab' );
-  var GravityForceLabA11yStrings = require( 'GRAVITY_FORCE_LAB/gravity-force-lab/GravityForceLabA11yStrings' );
-  var HBox = require( 'SCENERY/nodes/HBox' );
-  var KeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/KeyboardHelpSection' );
-  var HomeKeyNode = require( 'SCENERY_PHET/keyboard/HomeKeyNode' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var VBox = require( 'SCENERY/nodes/VBox' );
+  const ArrowKeyNode = require( 'SCENERY_PHET/keyboard/ArrowKeyNode' );
+  const EndKeyNode = require( 'SCENERY_PHET/keyboard/EndKeyNode' );
+  const GeneralKeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/GeneralKeyboardHelpSection' );
+  const gravityForceLab = require( 'GRAVITY_FORCE_LAB/gravityForceLab' );
+  const GravityForceLabA11yStrings = require( 'GRAVITY_FORCE_LAB/gravity-force-lab/GravityForceLabA11yStrings' );
+  const HBox = require( 'SCENERY/nodes/HBox' );
+  const KeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/KeyboardHelpSection' );
+  const HomeKeyNode = require( 'SCENERY_PHET/keyboard/HomeKeyNode' );
+  const VBox = require( 'SCENERY/nodes/VBox' );
 
   // strings
-  var changeMassHeadingString = require( 'string!GRAVITY_FORCE_LAB/changeMassHeading' );
-  var changeMassInLargerStepsString = require( 'string!GRAVITY_FORCE_LAB/changeMassInLargerSteps' );
-  var changeMassInSmallerStepsString = require( 'string!GRAVITY_FORCE_LAB/changeMassInSmallerSteps' );
-  var decreaseMassString = require( 'string!GRAVITY_FORCE_LAB/decreaseMass' );
-  var increaseMassString = require( 'string!GRAVITY_FORCE_LAB/increaseMass' );
-  var jumpToLeftString = require( 'string!GRAVITY_FORCE_LAB/jumpToLeft' );
-  var jumpToMaximumMassString = require( 'string!GRAVITY_FORCE_LAB/jumpToMaximumMass' );
-  var jumpToMinimumMassString = require( 'string!GRAVITY_FORCE_LAB/jumpToMinimumMass' );
-  var jumpToRightString = require( 'string!GRAVITY_FORCE_LAB/jumpToRight' );
-  var moveInLargerStepsString = require( 'string!GRAVITY_FORCE_LAB/moveInLargerSteps' );
-  var moveInSmallerStepsString = require( 'string!GRAVITY_FORCE_LAB/moveInSmallerSteps' );
-  var moveSphereLabelString = require( 'string!GRAVITY_FORCE_LAB/moveSphereLabel' );
-  var moveSpheresHeadingString = require( 'string!GRAVITY_FORCE_LAB/moveSpheresHeading' );
+  const changeMassHeadingString = require( 'string!GRAVITY_FORCE_LAB/changeMassHeading' );
+  const changeMassInLargerStepsString = require( 'string!GRAVITY_FORCE_LAB/changeMassInLargerSteps' );
+  const changeMassInSmallerStepsString = require( 'string!GRAVITY_FORCE_LAB/changeMassInSmallerSteps' );
+  const decreaseMassString = require( 'string!GRAVITY_FORCE_LAB/decreaseMass' );
+  const increaseMassString = require( 'string!GRAVITY_FORCE_LAB/increaseMass' );
+  const jumpToLeftString = require( 'string!GRAVITY_FORCE_LAB/jumpToLeft' );
+  const jumpToMaximumMassString = require( 'string!GRAVITY_FORCE_LAB/jumpToMaximumMass' );
+  const jumpToMinimumMassString = require( 'string!GRAVITY_FORCE_LAB/jumpToMinimumMass' );
+  const jumpToRightString = require( 'string!GRAVITY_FORCE_LAB/jumpToRight' );
+  const moveInLargerStepsString = require( 'string!GRAVITY_FORCE_LAB/moveInLargerSteps' );
+  const moveInSmallerStepsString = require( 'string!GRAVITY_FORCE_LAB/moveInSmallerSteps' );
+  const moveSphereLabelString = require( 'string!GRAVITY_FORCE_LAB/moveSphereLabel' );
+  const moveSpheresHeadingString = require( 'string!GRAVITY_FORCE_LAB/moveSpheresHeading' );
 
   // a11y strings
-  var moveSphereDescriptionString = GravityForceLabA11yStrings.moveSphereDescription.value;
-  var moveInSmallerStepsDescriptionString = GravityForceLabA11yStrings.moveInSmallerStepsDescription.value;
-  var moveInLargerStepsDescriptionString = GravityForceLabA11yStrings.moveInLargerStepsDescription.value;
-  var jumpToLeftDescriptionString = GravityForceLabA11yStrings.jumpToLeftDescription.value;
-  var jumpToRightDescriptionString = GravityForceLabA11yStrings.jumpToRightDescription.value;
-  var increaseMassDescriptionString = GravityForceLabA11yStrings.increaseMassDescription.value;
-  var decreaseMassDescriptionString = GravityForceLabA11yStrings.decreaseMassDescription.value;
-  var changeMassInLargerStepsDescriptionString = GravityForceLabA11yStrings.changeMassInLargerStepsDescription.value;
-  var changeMassInSmallerStepsDescriptionString = GravityForceLabA11yStrings.changeMassInSmallerStepsDescription.value;
-  var jumpToMaximumMassDescriptionString = GravityForceLabA11yStrings.jumpToMaximumMassDescription.value;
-  var jumpToMinimumMassDescriptionString = GravityForceLabA11yStrings.jumpToMinimumMassDescription.value;
+  const moveSphereDescriptionString = GravityForceLabA11yStrings.moveSphereDescription.value;
+  const moveInSmallerStepsDescriptionString = GravityForceLabA11yStrings.moveInSmallerStepsDescription.value;
+  const moveInLargerStepsDescriptionString = GravityForceLabA11yStrings.moveInLargerStepsDescription.value;
+  const jumpToLeftDescriptionString = GravityForceLabA11yStrings.jumpToLeftDescription.value;
+  const jumpToRightDescriptionString = GravityForceLabA11yStrings.jumpToRightDescription.value;
+  const increaseMassDescriptionString = GravityForceLabA11yStrings.increaseMassDescription.value;
+  const decreaseMassDescriptionString = GravityForceLabA11yStrings.decreaseMassDescription.value;
+  const changeMassInLargerStepsDescriptionString = GravityForceLabA11yStrings.changeMassInLargerStepsDescription.value;
+  const changeMassInSmallerStepsDescriptionString = GravityForceLabA11yStrings.changeMassInSmallerStepsDescription.value;
+  const jumpToMaximumMassDescriptionString = GravityForceLabA11yStrings.jumpToMaximumMassDescription.value;
+  const jumpToMinimumMassDescriptionString = GravityForceLabA11yStrings.jumpToMinimumMassDescription.value;
 
   // helper functions that return icons for the dialog
-  var ICON_CREATOR = {
-    home: function() {
+  const ICON_CREATOR = {
+    home: () => {
       return new HomeKeyNode();
     },
-    end: function() {
+    end: () => {
       return new EndKeyNode();
     },
-    leftRight: function() {
+    leftRight: () => {
       return KeyboardHelpSection.leftRightArrowKeysRowIcon();
     },
-    downOrLeft: function() {
+    downOrLeft: () => {
       return KeyboardHelpSection.iconOrIcon( new ArrowKeyNode( 'down' ), new ArrowKeyNode( 'left' ) );
     },
-    upOrRight: function() {
+    upOrRight: () => {
       return KeyboardHelpSection.iconOrIcon( new ArrowKeyNode( 'up' ), new ArrowKeyNode( 'right' ) );
     },
-    pageUpPageDown: function() {
+    pageUpPageDown: () => {
       return KeyboardHelpSection.pageUpPageDownRowIcon();
     },
-    shiftPlusArrows: function() {
+    shiftPlusArrows: () => {
       return KeyboardHelpSection.shiftPlusIcon( KeyboardHelpSection.leftRightArrowKeysRowIcon() );
     },
-    shiftPlusAllArrows: function() {
+    shiftPlusAllArrows: () => {
       return KeyboardHelpSection.shiftPlusIcon( KeyboardHelpSection.arrowKeysRowIcon() );
     }
   };
 
-  /**
-   * Constructor.
-   *
-   * @param {Tandem} tandem
-   * @constructor
-   */
-  function GravityForceLabKeyboardHelpContent( tandem ) {
-
-    // Mass movement help dialog section
-    // move mass section
-    var moveMassRow = this.constructRow( moveSphereLabelString, moveSphereDescriptionString, 'leftRight' );
-
-    var moveSmallStepsRow = this.constructRow( moveInSmallerStepsString, moveInSmallerStepsDescriptionString, 'shiftPlusArrows' );
-    // 'move in larger steps' section
-    var moveLargeStepsRow = this.constructRow( moveInLargerStepsString, moveInLargerStepsDescriptionString, 'pageUpPageDown' );
-
-    // 'jump to left' section
-    var jumpLeftRow = this.constructRow( jumpToLeftString, jumpToLeftDescriptionString, 'home' );
-
-    // 'jump to right' section
-    var jumpRightRow = this.constructRow( jumpToRightString, jumpToRightDescriptionString, 'end' );
-
-    var moveMassRows = [ moveMassRow, moveSmallStepsRow, moveLargeStepsRow, jumpLeftRow, jumpRightRow ];
-    var moveMassHelpSection = new KeyboardHelpSection( moveSpheresHeadingString, moveMassRows );
-
-    // Mass adjustment help section
-    var increaseMassRow = this.constructRow( increaseMassString, increaseMassDescriptionString, 'upOrRight' );
-    var decreaseMassRow = this.constructRow( decreaseMassString, decreaseMassDescriptionString, 'downOrLeft' );
-    var changeMassSmallStepsRow = this.constructRow( changeMassInSmallerStepsString, changeMassInSmallerStepsDescriptionString, 'shiftPlusAllArrows' );
-    var changeMassLargeStepsRow = this.constructRow( changeMassInLargerStepsString, changeMassInLargerStepsDescriptionString, 'pageUpPageDown' );
-    var jumpToMinMassRow = this.constructRow( jumpToMinimumMassString, jumpToMinimumMassDescriptionString, 'home' );
-    var jumpToMaxMassRow = this.constructRow( jumpToMaximumMassString, jumpToMaximumMassDescriptionString, 'end' );
-
-    var adjustMassRows = [ increaseMassRow, decreaseMassRow, changeMassSmallStepsRow, changeMassLargeStepsRow, jumpToMinMassRow, jumpToMaxMassRow ];
-    var adjustMassHelpSection = new KeyboardHelpSection( changeMassHeadingString, adjustMassRows );
-
-    // align icons for the mass movement and adjustment sections
-    KeyboardHelpSection.alignHelpSectionIcons( [ moveMassHelpSection, adjustMassHelpSection ] );
-
-    var generalNavigationHelpSection = new GeneralKeyboardHelpSection();
-
-    var leftContent = new VBox( {
-      children: [ moveMassHelpSection, adjustMassHelpSection ],
-      align: 'top',
-      spacing: 30
-    } );
-
-    var rightContent = new VBox( {
-      children: [ generalNavigationHelpSection ],
-      align: 'top',
-      spacing: 30
-    } );
-
-    HBox.call( this, {
-      children: [ leftContent, rightContent ],
-      align: 'top',
-      spacing: 30,
-      tandem: tandem
-    } );
-  }
-
-  gravityForceLab.register( 'GravityForceLabKeyboardHelpContent', GravityForceLabKeyboardHelpContent );
-
-  return inherit( HBox, GravityForceLabKeyboardHelpContent, {
+  class GravityForceLabKeyboardHelpContent extends HBox {
 
     /**
-     * Construct a row for the help dialog, assembling a label with an icon using HelpSection. Usages will look like:
-     * this.constructRow( 'jump to the end', 'end' );
-     *
-     * @param  {string} labelString - the text label for the row (visual)
-     * @param  {string} descriptionString - must be one of the keys in ICON_CREATOR
-     * @param  {string} iconID - must be one of ICON_CREATOR keys, see that above
+     * @param {Tandem} tandem
      */
-    constructRow: function( labelString, descriptionString, iconID ) {
-      var iconNode = ICON_CREATOR[ iconID ]();
-      return KeyboardHelpSection.labelWithIcon( labelString, iconNode, descriptionString );
+    constructor( tandem ) {
+
+      // Mass movement help dialog section
+      // move mass section
+      const moveMassRow = constructRow( moveSphereLabelString, moveSphereDescriptionString, 'leftRight' );
+
+      const moveSmallStepsRow = constructRow( moveInSmallerStepsString, moveInSmallerStepsDescriptionString, 'shiftPlusArrows' );
+      // 'move in larger steps' section
+      const moveLargeStepsRow = constructRow( moveInLargerStepsString, moveInLargerStepsDescriptionString, 'pageUpPageDown' );
+
+      // 'jump to left' section
+      const jumpLeftRow = constructRow( jumpToLeftString, jumpToLeftDescriptionString, 'home' );
+
+      // 'jump to right' section
+      const jumpRightRow = constructRow( jumpToRightString, jumpToRightDescriptionString, 'end' );
+
+      const moveMassRows = [ moveMassRow, moveSmallStepsRow, moveLargeStepsRow, jumpLeftRow, jumpRightRow ];
+      const moveMassHelpSection = new KeyboardHelpSection( moveSpheresHeadingString, moveMassRows );
+
+      // Mass adjustment help section
+      const increaseMassRow = constructRow( increaseMassString, increaseMassDescriptionString, 'upOrRight' );
+      const decreaseMassRow = constructRow( decreaseMassString, decreaseMassDescriptionString, 'downOrLeft' );
+      const changeMassSmallStepsRow = constructRow( changeMassInSmallerStepsString, changeMassInSmallerStepsDescriptionString, 'shiftPlusAllArrows' );
+      const changeMassLargeStepsRow = constructRow( changeMassInLargerStepsString, changeMassInLargerStepsDescriptionString, 'pageUpPageDown' );
+      const jumpToMinMassRow = constructRow( jumpToMinimumMassString, jumpToMinimumMassDescriptionString, 'home' );
+      const jumpToMaxMassRow = constructRow( jumpToMaximumMassString, jumpToMaximumMassDescriptionString, 'end' );
+
+      const adjustMassRows = [ increaseMassRow, decreaseMassRow, changeMassSmallStepsRow, changeMassLargeStepsRow, jumpToMinMassRow, jumpToMaxMassRow ];
+      const adjustMassHelpSection = new KeyboardHelpSection( changeMassHeadingString, adjustMassRows );
+
+      // align icons for the mass movement and adjustment sections
+      KeyboardHelpSection.alignHelpSectionIcons( [ moveMassHelpSection, adjustMassHelpSection ] );
+
+      const generalNavigationHelpSection = new GeneralKeyboardHelpSection();
+
+      const leftContent = new VBox( {
+        children: [ moveMassHelpSection, adjustMassHelpSection ],
+        align: 'top',
+        spacing: 30
+      } );
+
+      const rightContent = new VBox( {
+        children: [ generalNavigationHelpSection ],
+        align: 'top',
+        spacing: 30
+      } );
+
+      super( {
+        children: [ leftContent, rightContent ],
+        align: 'top',
+        spacing: 30,
+        tandem: tandem
+      } );
     }
-  } );
+  }
+
+  /**
+   * Construct a row for the help dialog, assembling a label with an icon using HelpSection. Usages will look like:
+   * constructRow( 'jump to the end', 'end' );
+   *
+   * @param {string} labelString - the text label for the row (visual)
+   * @param {string} descriptionString - must be one of the keys in ICON_CREATOR
+   * @param {string} iconID - must be one of ICON_CREATOR keys, see that above
+   */
+  const constructRow = ( labelString, descriptionString, iconID ) => {
+    const iconNode = ICON_CREATOR[ iconID ]();
+    return KeyboardHelpSection.labelWithIcon( labelString, iconNode, descriptionString );  
+  };
+
+  return gravityForceLab.register( 'GravityForceLabKeyboardHelpContent', GravityForceLabKeyboardHelpContent );
 } );
