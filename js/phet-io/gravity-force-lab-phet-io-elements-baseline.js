affe533aaf6084cd112f7d3ce45864d3b786fa7a
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 11:31:52 -0800

    update baseline files, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/gravity-force-lab-phet-io-elements-baseline.js
+++ js/phet-io/gravity-force-lab-phet-io-elements-baseline.js
@@ -4899,7 +4899,7 @@ window.phet.phetio.phetioElementsBaseline =
     "gravityForceLab.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
