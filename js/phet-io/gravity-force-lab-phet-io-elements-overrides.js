c2e1ed0a3a41b6a79175e7a8894c8569f1bc855b
zepumph <michael.kauzmann@colorado.edu>
2019-08-23 12:50:01 -0800

    add first pass at phetioFeatured overrides, https://github.com/phetsims/gravity-force-lab/issues/174

--- js/phet-io/gravity-force-lab-phet-io-elements-overrides.js
+++ js/phet-io/gravity-force-lab-phet-io-elements-overrides.js
@@ -1,2 +1,100 @@
 /* eslint-disable */
-window.phet.phetio.phetioElementsOverrides = {};
+window.phet.phetio.phetioElementsOverrides =
+  {
+    "gravityForceLab.gravityForceLabScreen.model.constantRadiusProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.model.forceProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.model.mass1.positionProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.model.mass1.valueProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.model.mass2.positionProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.model.mass2.valueProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.model.rulerPositionProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.model.scientificNotationProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.model.separationProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.model.showForceValuesProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.mass1Node.centerOfMassLineNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.mass1Node.forceDisplayNode.arrowNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.mass1Node.forceDisplayNode.forceText.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.mass1Node.forceDisplayNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.mass1Node.textProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.mass2Node.centerOfMassLineNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.mass2Node.forceDisplayNode.arrowNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.mass2Node.forceDisplayNode.forceText.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.mass2Node.forceDisplayNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.mass2Node.textProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.massControl1.numberControl.numberDisplay.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.massControl1.numberControl.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.massControl1.numberControl.titleNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.massControl1.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.massControl2.numberControl.numberDisplay.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.massControl2.numberControl.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.massControl2.numberControl.titleNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.massControl2.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.parameterControlPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.ruler.opacityProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.ruler.pickableProperty": {
+      "phetioFeatured": true
+    },
+    "gravityForceLab.gravityForceLabScreen.view.ruler.visibleProperty": {
+      "phetioFeatured": true
+    }
+  };
