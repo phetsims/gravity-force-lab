0a0e34e39dac2f6384365209fe1413cc89f22398
zepumph <michael.kauzmann@colorado.edu>
2019-09-11 14:17:51 -0800

    grunt update, https://github.com/phetsims/phet-io-wrappers/issues/296

--- js/phet-io/gravity-force-lab-phet-io-elements-baseline.js
+++ js/phet-io/gravity-force-lab-phet-io-elements-baseline.js
@@ -3683,7 +3683,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "NodeIO"
+      "phetioTypeName": "ResetAllButtonIO"
     },
     "gravityForceLab.gravityForceLabScreen.view.resetAllButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
