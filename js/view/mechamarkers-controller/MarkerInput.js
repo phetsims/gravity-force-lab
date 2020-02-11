bf86a5f91ddca75b6317a97f0fa4255927402170
zepumph <michael.kauzmann@colorado.edu>
2020-02-11 13:59:44 -0900

    add slider marker and wire it up to both masses and object1 position, https://github.com/phetsims/gravity-force-lab/issues/249

--- js/view/mechamarkers-controller/MarkerInput.js
+++ js/view/mechamarkers-controller/MarkerInput.js
@@ -9,10 +9,11 @@ define( require => {
 
   // modules
   const gravityForceLab = require( 'GRAVITY_FORCE_LAB/gravityForceLab' );
+  const merge = require( 'PHET_CORE/merge' );
   const timer = require( 'AXON/timer' );
 
   // constants
-  const MARKER_NUMBER = 18; // change this marker number to toggle constant radius
+  const CONSTANT_SIZE_MARKER = 18; // change this marker number to toggle constant radius
 
   class MarkerInput {
 
@@ -23,16 +24,35 @@ define( require => {
 
       let previousButtonPresent = false;
 
+      const mass1PositionSlider = new SliderMarker( 'mass1Position', 'slider',
+        model.object1.positionProperty, {
+          range: model.object1.enabledRangeProperty
+        } );
+      // const mass2PositionSlider = new SliderMarker( 'mass1Position', 'slider',
+      //   model.object2.positionProperty, {
+      //     range: model.object2.enabledRangeProperty
+      //   } );
+      const mass1MassSlider = new SliderMarker( 'mass1Position', 'slider',
+        model.object1.valueProperty );
+      const mass2MassSlider = new SliderMarker( 'mass1Position', 'slider',
+        model.object2.valueProperty );
+
       function update() {
 
         // Mechamarkers stuff
         Mechamarkers.update( Date.now() );
 
+        mass1PositionSlider.update();
 
-        if ( Mechamarkers.getMarker( MARKER_NUMBER ).present !== previousButtonPresent ) {
+        // This breaks right now because both positions will be set to the same value
+        // mass2PositionSlider.update();
+        mass1MassSlider.update();
+        mass2MassSlider.update();
 
-          Mechamarkers.getMarker( MARKER_NUMBER ).present && model.constantRadiusProperty.toggle();
-          previousButtonPresent = Mechamarkers.getMarker( MARKER_NUMBER ).present;
+        if ( Mechamarkers.getMarker( CONSTANT_SIZE_MARKER ).present !== previousButtonPresent ) {
+
+          Mechamarkers.getMarker( CONSTANT_SIZE_MARKER ).present && model.constantRadiusProperty.toggle();
+          previousButtonPresent = Mechamarkers.getMarker( CONSTANT_SIZE_MARKER ).present;
         }
       }
 
@@ -42,6 +62,52 @@ define( require => {
       const ctx = canvas.getContext( '2d' );
       document.body.appendChild( canvas );
       Mechamarkers.init( canvas, ctx );
+    }
+  }
+
+  // Private class that supports listening for a single slider in an input group
+  class SliderMarker {
+
+    /**
+     *
+     * @param {string} inputGroup
+     * @param {string} inputName
+     * @param {Property.<number>} property
+     * @param {Object} [options]
+     */
+    constructor( inputGroup, inputName, property, options ) {
+      options = merge( {
+
+        // {Range} By default use the range from the Property
+        range: property.range
+      }, options );
+
+      assert && assert( options.range, 'options.range needs to be provided, or Property.range supplied.' );
+
+      // private
+      this.inputGroup = inputGroup;
+      this.inputName = inputName;
+      this.property = property;
+      this.range = options.range;
+    }
+
+    /**
+     * Set the value based on the range of the property
+     * @param {number} inputValue - output from mechamarkers, between 0 and 1
+     */
+    setProperty( inputValue ) {
+      this.property.value = inputValue * this.property.range.getLength() + this.property.range.min;
+    }
+
+    /**
+     * Update the state of the slider, setting the Property value if applicable
+     */
+    update() {
+      const group = Mechamarkers.getGroup( this.inputGroup );
+      if ( group && group.anchor.present ) {
+        const value = group.getInput( this.inputName ).val;
+        this.setProperty( value );
+      }
     }
   }
 
