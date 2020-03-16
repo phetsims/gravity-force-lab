f172b89a7707f8ceab2dbbff296d84e5ca06a248
zepumph <michael.kauzmann@colorado.edu>
2020-03-16 14:41:52 -0800

    remove ResetAllButtonIO.js, https://github.com/phetsims/scenery-phet/issues/572

--- js/phet-io/gravity-force-lab-phet-io-elements-baseline.js
+++ js/phet-io/gravity-force-lab-phet-io-elements-baseline.js
@@ -5355,7 +5355,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "ResetAllButtonIO"
+      "phetioTypeName": "NodeIO"
     },
     "gravityForceLab.gravityForceLabScreen.view.resetAllButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
