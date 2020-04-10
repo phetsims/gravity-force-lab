a984e4008bb25ea2ea760e4dc65dd60bbfa7443a
pixelzoom <cmalley@pixelzoom.com>
2020-04-10 13:45:51 -0600

    revise IO Type doc for primitive core types, https://github.com/phetsims/tandem/issues/165

--- js/phet-io/gravity-force-lab-types.js
+++ js/phet-io/gravity-force-lab-types.js
@@ -212,7 +212,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "ArrayIO<StringIO>"
     },
     "BooleanIO": {
-      "documentation": "Wrapper for the built-in JS boolean type (true/false)",
+      "documentation": "IO Type for Javascript's boolean primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
@@ -758,7 +758,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "NumberDisplayIO"
     },
     "NumberIO": {
-      "documentation": "Wrapper for the built-in JS number type (floating point, but also represents integers)",
+      "documentation": "IO Type for Javascript's number primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
@@ -1581,7 +1581,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "SliderIO"
     },
     "StringIO": {
-      "documentation": "Wrapper for the built-in JS string type",
+      "documentation": "IO Type for Javascript's string primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
