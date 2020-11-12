de1b023634c4517486a33ad6d4f0a985c4396fd1
zepumph <michael.kauzmann@colorado.edu>
2020-11-12 13:05:52 -0900

    convert to es6, https://github.com/phetsims/tasks/issues/1051

--- js/model/GravityForceLabModel.js
+++ js/model/GravityForceLabModel.js
@@ -15,7 +15,6 @@ import Vector2 from '../../../dot/js/Vector2.js';
 import Vector2Property from '../../../dot/js/Vector2Property.js';
 import ForceValuesDisplayEnum from '../../../inverse-square-law-common/js/model/ForceValuesDisplayEnum.js';
 import ISLCModel from '../../../inverse-square-law-common/js/model/ISLCModel.js';
-import inherit from '../../../phet-core/js/inherit.js';
 import PhysicalConstants from '../../../phet-core/js/PhysicalConstants.js';
 import gravityForceLab from '../gravityForceLab.js';
 import GravityForceLabConstants from '../GravityForceLabConstants.js';
@@ -27,72 +26,73 @@ const MASS_OPTIONS = {
   rightObjectBoundary: GravityForceLabConstants.PULL_POSITION_RANGE.max
 };
 
-/**
- * @param {Tandem} tandem
- * @constructor
- */
-function GravityForceLabModel( tandem ) {
+class GravityForceLabModel extends ISLCModel {
 
-  // @public
-  this.rulerPositionProperty = new Vector2Property( new Vector2( 0, -1 ), {
-    tandem: tandem.createTandem( 'rulerPositionProperty' ),
-    phetioDocumentation: 'The position of the ruler in model coordinates'
-  } );
+  /**
+   * @param {Tandem} tandem
+   */
+  constructor( tandem ) {
 
-  // @public
-  this.constantRadiusProperty = new BooleanProperty( false, {
-    tandem: tandem.createTandem( 'constantRadiusProperty' ),
-    phetioDocumentation: 'Use this to toggle if the masses have a constantly sized radius, even when the mass changes.'
-  } );
-
-  // pass initial masses and positions into the model
-  const massValue1 = 100; // mass in kg
-  const massValue2 = 400; // mass in kg
-
-  const position1 = -3; // in meters
-  const position2 = 1; // in meters
-
-  const baseColor1 = GravityForceLabConstants.MASS_BLUE_COLOR;
-  const baseColor2 = GravityForceLabConstants.MASS_RED_COLOR;
-
-  const density = 150; // in kg/m^3
-
-  const mass1 = new Mass( massValue1, position1, GravityForceLabConstants.MASS_RANGE, density,
-    this.constantRadiusProperty, baseColor1, tandem.createTandem( 'mass1' ), MASS_OPTIONS );
-  const mass2 = new Mass( massValue2, position2, GravityForceLabConstants.MASS_RANGE, density,
-    this.constantRadiusProperty, baseColor2, tandem.createTandem( 'mass2' ), MASS_OPTIONS );
-
-  // leverage ISLCModel, in "mass" mode
-  ISLCModel.call( this, PhysicalConstants.GRAVITATIONAL_CONSTANT, mass1, mass2,
-    GravityForceLabConstants.PULL_POSITION_RANGE, tandem, {
-      snapObjectsToNearest: 0.1 // in meters
+    const constantRadiusProperty = new BooleanProperty( false, {
+      tandem: tandem.createTandem( 'constantRadiusProperty' ),
+      phetioDocumentation: 'Use this to toggle if the masses have a constantly sized radius, even when the mass changes.'
     } );
 
+    // pass initial masses and positions into the model
+    const massValue1 = 100; // mass in kg
+    const massValue2 = 400; // mass in kg
+
+    const position1 = -3; // in meters
+    const position2 = 1; // in meters
+
+    const baseColor1 = GravityForceLabConstants.MASS_BLUE_COLOR;
+    const baseColor2 = GravityForceLabConstants.MASS_RED_COLOR;
+
+    const density = 150; // in kg/m^3
+
+    const mass1 = new Mass( massValue1, position1, GravityForceLabConstants.MASS_RANGE, density,
+      constantRadiusProperty, baseColor1, tandem.createTandem( 'mass1' ), MASS_OPTIONS );
+    const mass2 = new Mass( massValue2, position2, GravityForceLabConstants.MASS_RANGE, density,
+      constantRadiusProperty, baseColor2, tandem.createTandem( 'mass2' ), MASS_OPTIONS );
+
+    // leverage ISLCModel, in "mass" mode
+    super( PhysicalConstants.GRAVITATIONAL_CONSTANT, mass1, mass2,
+      GravityForceLabConstants.PULL_POSITION_RANGE, tandem, {
+        snapObjectsToNearest: 0.1 // in meters
+      } );
+
+    // @public
+    this.constantRadiusProperty = constantRadiusProperty;
+
+    // @public
+    this.rulerPositionProperty = new Vector2Property( new Vector2( 0, -1 ), {
+      tandem: tandem.createTandem( 'rulerPositionProperty' ),
+      phetioDocumentation: 'The position of the ruler in model coordinates'
+    } );
+
+    // @public
+    this.forceValuesDisplayProperty = new EnumerationProperty( ForceValuesDisplayEnum, ForceValuesDisplayEnum.DECIMAL, {
+      tandem: tandem.createTandem( 'forceValuesDisplayProperty' ),
+      phetioDocumentation: 'This determines the display type for the force values: in decimal or scientific ' +
+                           'notation, and also hidden.'
+    } );
+
+    // ISLC code listens substantially to showForceValuesProperty, so keep that in sync as the display type changes.
+    this.forceValuesDisplayProperty.lazyLink( newValue => {
+      this.showForceValuesProperty.value = newValue === ForceValuesDisplayEnum.DECIMAL ||
+                                           newValue === ForceValuesDisplayEnum.SCIENTIFIC;
+    } );
+  }
+
   // @public
-  this.forceValuesDisplayProperty = new EnumerationProperty( ForceValuesDisplayEnum, ForceValuesDisplayEnum.DECIMAL, {
-    tandem: tandem.createTandem( 'forceValuesDisplayProperty' ),
-    phetioDocumentation: 'This determines the display type for the force values: in decimal or scientific ' +
-                         'notation, and also hidden.'
-  } );
-
-  // ISLC code listens substantially to showForceValuesProperty, so keep that in sync as the display type changes.
-  this.forceValuesDisplayProperty.lazyLink( newValue => {
-    this.showForceValuesProperty.value = newValue === ForceValuesDisplayEnum.DECIMAL ||
-                                         newValue === ForceValuesDisplayEnum.SCIENTIFIC;
-  } );
-}
-
-gravityForceLab.register( 'GravityForceLabModel', GravityForceLabModel );
-
-inherit( ISLCModel, GravityForceLabModel, {
-
-  // @public
-  reset: function() {
+  reset() {
     this.rulerPositionProperty.reset();
     this.forceValuesDisplayProperty.reset();
     this.constantRadiusProperty.reset();
-    ISLCModel.prototype.reset.call( this );
+    super.reset();
   }
-} );
+}
+
+gravityForceLab.register( 'GravityForceLabModel', GravityForceLabModel );
 
 export default GravityForceLabModel;
