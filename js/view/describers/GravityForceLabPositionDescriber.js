5993821a779f528e92dbf2634dc8dba8b6a83aea
phet-dev <phetadmin@gmail.com>
2021-05-25 15:51:10 -0600

    update copyright dates from daily grunt work

--- js/view/describers/GravityForceLabPositionDescriber.js
+++ js/view/describers/GravityForceLabPositionDescriber.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * This describer is responsible for all gravity-force-lab specific string forming related to position.
