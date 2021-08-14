0492b7bd9f86217eafa2216563bfac416a417d3f
Jesse <jesse.greenberg@colorado.edu>
2021-08-14 19:18:19 -0400

    Implement getDistanceIndex in PositionDescriber for all ISLC sims, normalize the distance between objects to describe, see https://github.com/phetsims/gravity-force-lab-basics/issues/206

--- js/view/describers/GravityForceLabPositionDescriber.js
+++ js/view/describers/GravityForceLabPositionDescriber.js
@@ -27,47 +27,6 @@ class GravityForceLabPositionDescriber extends PositionDescriber {
 
     super( model, object1Label, object2Label, options );
   }
-
-  /**
-   * These empirically determined values were designed, see https://docs.google.com/document/d/1-37qAgde2XrlXBQae2SgjartM35_EnzDD9pdtd3nXAM/edit#heading=h.nhqxjbby3dgu
-   * @param {number} distance
-   * @param {number} numberOfRegions - for crosscheck
-   * @returns {number}
-   * @protected
-   * @override
-   */
-  getDistanceIndex( distance, numberOfRegions ) {
-    assert && assert( distance >= 0, 'Distance between spheres should always be positive.' );
-    assert && assert( numberOfRegions === 9, 'If numberOfRegions changes, this function should too.' );
-
-    // Though this is technically the min, the model can be set to the wrong value, and the rely on that "snap"
-    // functionality to bring the distance back into the correct range on next model step. So support values less than.
-    if ( distance <= 0.6 ) {
-      return 8;
-    }
-    if ( distance < 1.6 ) {
-      return 7;
-    }
-    if ( distance < 2.6 ) {
-      return 6;
-    }
-    if ( distance < 3.6 ) {
-      return 5;
-    }
-    if ( distance < 5.6 ) {
-      return 4;
-    }
-    if ( distance < 7.6 ) {
-      return 3;
-    }
-    if ( distance < 8.6 ) {
-      return 2;
-    }
-    if ( distance < 9.6 ) {
-      return 1;
-    }
-    return 0;
-  }
 }
 
 gravityForceLab.register( 'GravityForceLabPositionDescriber', GravityForceLabPositionDescriber );
