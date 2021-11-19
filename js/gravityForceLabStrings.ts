2e7c2e3bd6f1cbfe429b8b66dbe05ed26c29d97c
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:44 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/gravityForceLabStrings.ts
@@ -0,0 +1,181 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import gravityForceLab from './gravityForceLab.js';
+
+type StringsType = {
+  'gravity-force-lab': {
+    'title': string
+  },
+  'mass1': string,
+  'mass2': string,
+  'units': {
+    'kg': string
+  },
+  'mass1Abbreviated': string,
+  'mass2Abbreviated': string,
+  'constantSize': string,
+  'moveSpheresHeading': string,
+  'moveSphereLabel': string,
+  'moveInSmallerSteps': string,
+  'moveInLargerSteps': string,
+  'jumpToLeft': string,
+  'jumpToRight': string,
+  'changeMassHeading': string,
+  'changeMassLabel': string,
+  'changeMassInLargerSteps': string,
+  'changeMassInSmallerSteps': string,
+  'jumpToMaximumMass': string,
+  'jumpToMinimumMass': string,
+  'moveGrabbedRuler': string,
+  'moveOrJumpGrabbedRuler': string,
+  'jumpStartOfSphere': string,
+  'jumpHome': string,
+  'a11y': {
+    'screenSummary': {
+      'playAreaOverview': string,
+      'playAreaControls': string,
+      'secondaryDescription': string,
+      'simStateListLabel': string,
+      'massValuesAndComparisonSummaryPattern': string
+    },
+    'blueSpherePattern': string,
+    'redSpherePattern': string,
+    'sizeAndDistancePattern': string,
+    'sizePattern': string,
+    'micronewtons': string,
+    'mass': string,
+    'qualitative': {
+      'objectsRelativeSizePattern': string,
+      'massAndUnitPattern': string,
+      'massAndForceClausesPattern': string,
+      'massChangeClausePattern': string,
+      'massChangesAndMovesClausePattern': string,
+      'massChangesMovesOtherClausePattern': string
+    },
+    'sentencePattern': string,
+    'relativeMassSize': {
+      'muchMuchSmallerThan': string,
+      'halfTheSizeOf': string,
+      'muchSmallerThan': string,
+      'smallerButComparableTo': string,
+      'sameSizeAs': string,
+      'largerButComparableTo': string,
+      'muchLargerThan': string,
+      'twiceTheSizeOf': string,
+      'muchMuchLargerThan': string
+    },
+    'relativeMassSizeCapitalized': {
+      'muchMuchSmallerThan': string,
+      'halfTheSizeOf': string,
+      'muchSmallerThan': string,
+      'smallerButComparableTo': string,
+      'sameSizeAs': string,
+      'largerButComparableTo': string,
+      'muchLargerThan': string,
+      'twiceTheSizeOf': string,
+      'muchMuchLargerThan': string
+    },
+    'relativeMassDensity': {
+      'notDenseComparedTo': string,
+      'halfAsDenseAs': string,
+      'muchLessDenseThan': string,
+      'lessDenseButComparableTo': string,
+      'asDenseAs': string,
+      'denseButComparableTo': string,
+      'muchDenseThan': string,
+      'twiceAsDenseAs': string,
+      'extremelyDenseComparedTo': string
+    },
+    'relativeMassDensityCapitalized': {
+      'notDenseComparedTo': string,
+      'halfAsDenseAs': string,
+      'muchLessDenseThan': string,
+      'lessDenseButComparableTo': string,
+      'asDenseAs': string,
+      'denseButComparableTo': string,
+      'muchDenseThan': string,
+      'twiceAsDenseAs': string,
+      'extremelyDenseComparedTo': string
+    },
+    'propertyChange': {
+      'massGetsSmaller': string,
+      'massGetsBigger': string,
+      'densityIncreases': string,
+      'densityDecreases': string
+    },
+    'controls': {
+      'massControlsLabel': string,
+      'massControlsHelpText': string,
+      'massControlsHelpTextDensity': string,
+      'constantSizeCheckboxHelpText': string,
+      'constantRadiusThinkDensityPattern': string,
+      'massMaxMinBorderTextWithForce': string,
+      'massMaxMinBorderTextWithoutForce': string
+    },
+    'keyboardHelp': {
+      'moveSphereDescription': string,
+      'moveInSmallerStepsDescription': string,
+      'moveInLargerStepsDescription': string,
+      'jumpToLeftDescription': string,
+      'jumpToRightDescription': string,
+      'changeMassPDOM': string,
+      'changeMassBasicsPDOM': string,
+      'changeMassInLargerStepsDescription': string,
+      'changeMassInSmallerStepsDescription': string,
+      'jumpToMaximumMassDescription': string,
+      'jumpToMinimumMassDescription': string,
+      'moveGrabbedRulerPDOM': string,
+      'moveInSmallerStepsPDOM': string,
+      'jumpStartOfSpherePDOM': string,
+      'jumpHomePDOM': string
+    },
+    'ruler': {
+      'regionAndDistancePattern': string,
+      'releaseAndExploreHint': string,
+      'grabbedAlertPattern': string,
+      'hintPattern': string,
+      'grabbedJumpKeyboardHint': string,
+      'jumpCenterKeyboardHint': string,
+      'gestureHint': string,
+      'keyboardReleaseHint': string,
+      'jumpCenterMassAlert': string,
+      'positions': {
+        'coveringM2': string,
+        'coveringM1': string,
+        'justAboveCenters': string,
+        'coveringCenters': string,
+        'justBelowCenters': string,
+        'inHomePosition': string,
+        'behindMassControls': string
+      }
+    },
+    'voicing': {
+      'briefPositionChangeInteractionPattern': string,
+      'briefMassChangeForceAlertWithValuePattern': string,
+      'briefMassChangeForceAlertPattern': string,
+      'briefDensityChangeForceAlertPattern': string,
+      'briefNewForcePattern': string,
+      'briefMassPushAlertPattern': string,
+      'briefMassChangeWithPushAlertPattern': string,
+      'briefMassChangeAlertPattern': string,
+      'biggerCapitalized': string,
+      'smallerCapitalized': string,
+      'more': string,
+      'less': string,
+      'levels': {
+        'distanceArrowPattern': string
+      }
+    }
+  }
+};
+
+const gravityForceLabStrings = getStringModule( 'GRAVITY_FORCE_LAB' ) as StringsType;
+
+gravityForceLab.register( 'gravityForceLabStrings', gravityForceLabStrings );
+
+export default gravityForceLabStrings;
