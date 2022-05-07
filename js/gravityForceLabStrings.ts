8eadf0fcd3d72733e127d884f14fa6009933a1e3
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:50 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/gravityForceLabStrings.ts
+++ js/gravityForceLabStrings.ts
@@ -9,166 +9,166 @@ import gravityForceLab from './gravityForceLab.js';
 
 type StringsType = {
   'gravity-force-lab': {
-    'title': string
-  },
-  'mass1': string,
-  'mass2': string,
+    'title': string;
+  };
+  'mass1': string;
+  'mass2': string;
   'units': {
-    'kg': string
-  },
-  'mass1Abbreviated': string,
-  'mass2Abbreviated': string,
-  'constantSize': string,
-  'moveSpheresHeading': string,
-  'moveSphereLabel': string,
-  'moveInSmallerSteps': string,
-  'moveInLargerSteps': string,
-  'jumpToLeft': string,
-  'jumpToRight': string,
-  'changeMassHeading': string,
-  'changeMassLabel': string,
-  'changeMassInLargerSteps': string,
-  'changeMassInSmallerSteps': string,
-  'jumpToMaximumMass': string,
-  'jumpToMinimumMass': string,
-  'moveGrabbedRuler': string,
-  'moveOrJumpGrabbedRuler': string,
-  'jumpStartOfSphere': string,
-  'jumpHome': string,
+    'kg': string;
+  };
+  'mass1Abbreviated': string;
+  'mass2Abbreviated': string;
+  'constantSize': string;
+  'moveSpheresHeading': string;
+  'moveSphereLabel': string;
+  'moveInSmallerSteps': string;
+  'moveInLargerSteps': string;
+  'jumpToLeft': string;
+  'jumpToRight': string;
+  'changeMassHeading': string;
+  'changeMassLabel': string;
+  'changeMassInLargerSteps': string;
+  'changeMassInSmallerSteps': string;
+  'jumpToMaximumMass': string;
+  'jumpToMinimumMass': string;
+  'moveGrabbedRuler': string;
+  'moveOrJumpGrabbedRuler': string;
+  'jumpStartOfSphere': string;
+  'jumpHome': string;
   'a11y': {
     'screenSummary': {
-      'playAreaOverview': string,
-      'playAreaControls': string,
-      'secondaryDescription': string,
-      'simStateListLabel': string,
-      'massValuesAndComparisonSummaryPattern': string
-    },
-    'blueSpherePattern': string,
-    'redSpherePattern': string,
-    'sizeAndDistancePattern': string,
-    'sizePattern': string,
-    'micronewtons': string,
-    'mass': string,
+      'playAreaOverview': string;
+      'playAreaControls': string;
+      'secondaryDescription': string;
+      'simStateListLabel': string;
+      'massValuesAndComparisonSummaryPattern': string;
+    };
+    'blueSpherePattern': string;
+    'redSpherePattern': string;
+    'sizeAndDistancePattern': string;
+    'sizePattern': string;
+    'micronewtons': string;
+    'mass': string;
     'qualitative': {
-      'objectsRelativeSizePattern': string,
-      'massAndUnitPattern': string,
-      'massAndForceClausesPattern': string,
-      'massChangeClausePattern': string,
-      'massChangesAndMovesClausePattern': string,
-      'massChangesMovesOtherClausePattern': string
-    },
-    'sentencePattern': string,
+      'objectsRelativeSizePattern': string;
+      'massAndUnitPattern': string;
+      'massAndForceClausesPattern': string;
+      'massChangeClausePattern': string;
+      'massChangesAndMovesClausePattern': string;
+      'massChangesMovesOtherClausePattern': string;
+    };
+    'sentencePattern': string;
     'relativeMassSize': {
-      'muchMuchSmallerThan': string,
-      'halfTheSizeOf': string,
-      'muchSmallerThan': string,
-      'smallerButComparableTo': string,
-      'sameSizeAs': string,
-      'largerButComparableTo': string,
-      'muchLargerThan': string,
-      'twiceTheSizeOf': string,
-      'muchMuchLargerThan': string
-    },
+      'muchMuchSmallerThan': string;
+      'halfTheSizeOf': string;
+      'muchSmallerThan': string;
+      'smallerButComparableTo': string;
+      'sameSizeAs': string;
+      'largerButComparableTo': string;
+      'muchLargerThan': string;
+      'twiceTheSizeOf': string;
+      'muchMuchLargerThan': string;
+    };
     'relativeMassSizeCapitalized': {
-      'muchMuchSmallerThan': string,
-      'halfTheSizeOf': string,
-      'muchSmallerThan': string,
-      'smallerButComparableTo': string,
-      'sameSizeAs': string,
-      'largerButComparableTo': string,
-      'muchLargerThan': string,
-      'twiceTheSizeOf': string,
-      'muchMuchLargerThan': string
-    },
+      'muchMuchSmallerThan': string;
+      'halfTheSizeOf': string;
+      'muchSmallerThan': string;
+      'smallerButComparableTo': string;
+      'sameSizeAs': string;
+      'largerButComparableTo': string;
+      'muchLargerThan': string;
+      'twiceTheSizeOf': string;
+      'muchMuchLargerThan': string;
+    };
     'relativeMassDensity': {
-      'notDenseComparedTo': string,
-      'halfAsDenseAs': string,
-      'muchLessDenseThan': string,
-      'lessDenseButComparableTo': string,
-      'asDenseAs': string,
-      'denseButComparableTo': string,
-      'muchDenseThan': string,
-      'twiceAsDenseAs': string,
-      'extremelyDenseComparedTo': string
-    },
+      'notDenseComparedTo': string;
+      'halfAsDenseAs': string;
+      'muchLessDenseThan': string;
+      'lessDenseButComparableTo': string;
+      'asDenseAs': string;
+      'denseButComparableTo': string;
+      'muchDenseThan': string;
+      'twiceAsDenseAs': string;
+      'extremelyDenseComparedTo': string;
+    };
     'relativeMassDensityCapitalized': {
-      'notDenseComparedTo': string,
-      'halfAsDenseAs': string,
-      'muchLessDenseThan': string,
-      'lessDenseButComparableTo': string,
-      'asDenseAs': string,
-      'denseButComparableTo': string,
-      'muchDenseThan': string,
-      'twiceAsDenseAs': string,
-      'extremelyDenseComparedTo': string
-    },
+      'notDenseComparedTo': string;
+      'halfAsDenseAs': string;
+      'muchLessDenseThan': string;
+      'lessDenseButComparableTo': string;
+      'asDenseAs': string;
+      'denseButComparableTo': string;
+      'muchDenseThan': string;
+      'twiceAsDenseAs': string;
+      'extremelyDenseComparedTo': string;
+    };
     'propertyChange': {
-      'massGetsSmaller': string,
-      'massGetsBigger': string,
-      'densityIncreases': string,
-      'densityDecreases': string
-    },
+      'massGetsSmaller': string;
+      'massGetsBigger': string;
+      'densityIncreases': string;
+      'densityDecreases': string;
+    };
     'controls': {
-      'massControlsLabel': string,
-      'massControlsHelpText': string,
-      'massControlsHelpTextDensity': string,
-      'constantSizeCheckboxHelpText': string,
-      'constantRadiusThinkDensityPattern': string,
-      'massMaxMinBorderTextWithForce': string,
-      'massMaxMinBorderTextWithoutForce': string
-    },
+      'massControlsLabel': string;
+      'massControlsHelpText': string;
+      'massControlsHelpTextDensity': string;
+      'constantSizeCheckboxHelpText': string;
+      'constantRadiusThinkDensityPattern': string;
+      'massMaxMinBorderTextWithForce': string;
+      'massMaxMinBorderTextWithoutForce': string;
+    };
     'keyboardHelp': {
-      'moveSphereDescription': string,
-      'moveInSmallerStepsDescription': string,
-      'moveInLargerStepsDescription': string,
-      'jumpToLeftDescription': string,
-      'jumpToRightDescription': string,
-      'changeMassPDOM': string,
-      'changeMassBasicsPDOM': string,
-      'changeMassInLargerStepsDescription': string,
-      'changeMassInSmallerStepsDescription': string,
-      'jumpToMaximumMassDescription': string,
-      'jumpToMinimumMassDescription': string,
-      'moveGrabbedRulerPDOM': string,
-      'moveInSmallerStepsPDOM': string,
-      'jumpStartOfSpherePDOM': string,
-      'jumpHomePDOM': string
-    },
+      'moveSphereDescription': string;
+      'moveInSmallerStepsDescription': string;
+      'moveInLargerStepsDescription': string;
+      'jumpToLeftDescription': string;
+      'jumpToRightDescription': string;
+      'changeMassPDOM': string;
+      'changeMassBasicsPDOM': string;
+      'changeMassInLargerStepsDescription': string;
+      'changeMassInSmallerStepsDescription': string;
+      'jumpToMaximumMassDescription': string;
+      'jumpToMinimumMassDescription': string;
+      'moveGrabbedRulerPDOM': string;
+      'moveInSmallerStepsPDOM': string;
+      'jumpStartOfSpherePDOM': string;
+      'jumpHomePDOM': string;
+    };
     'ruler': {
-      'regionAndDistancePattern': string,
-      'releaseAndExploreHint': string,
-      'grabbedAlertPattern': string,
-      'hintPattern': string,
-      'grabbedJumpKeyboardHint': string,
-      'jumpCenterKeyboardHint': string,
-      'gestureHint': string,
-      'keyboardReleaseHint': string,
-      'jumpCenterMassAlert': string,
+      'regionAndDistancePattern': string;
+      'releaseAndExploreHint': string;
+      'grabbedAlertPattern': string;
+      'hintPattern': string;
+      'grabbedJumpKeyboardHint': string;
+      'jumpCenterKeyboardHint': string;
+      'gestureHint': string;
+      'keyboardReleaseHint': string;
+      'jumpCenterMassAlert': string;
       'positions': {
-        'coveringM2': string,
-        'coveringM1': string,
-        'justAboveCenters': string,
-        'coveringCenters': string,
-        'justBelowCenters': string,
-        'inHomePosition': string,
-        'behindMassControls': string
+        'coveringM2': string;
+        'coveringM1': string;
+        'justAboveCenters': string;
+        'coveringCenters': string;
+        'justBelowCenters': string;
+        'inHomePosition': string;
+        'behindMassControls': string;
       }
-    },
+    };
     'voicing': {
-      'briefPositionChangeInteractionPattern': string,
-      'briefMassChangeForceAlertWithValuePattern': string,
-      'briefMassChangeForceAlertPattern': string,
-      'briefDensityChangeForceAlertPattern': string,
-      'briefNewForcePattern': string,
-      'briefMassPushAlertPattern': string,
-      'briefMassChangeWithPushAlertPattern': string,
-      'briefMassChangeAlertPattern': string,
-      'biggerCapitalized': string,
-      'smallerCapitalized': string,
-      'more': string,
-      'less': string,
+      'briefPositionChangeInteractionPattern': string;
+      'briefMassChangeForceAlertWithValuePattern': string;
+      'briefMassChangeForceAlertPattern': string;
+      'briefDensityChangeForceAlertPattern': string;
+      'briefNewForcePattern': string;
+      'briefMassPushAlertPattern': string;
+      'briefMassChangeWithPushAlertPattern': string;
+      'briefMassChangeAlertPattern': string;
+      'biggerCapitalized': string;
+      'smallerCapitalized': string;
+      'more': string;
+      'less': string;
       'levels': {
-        'distanceArrowPattern': string
+        'distanceArrowPattern': string;
       }
     }
   }
