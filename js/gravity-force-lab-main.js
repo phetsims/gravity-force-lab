ab03668e7a2371bfc0379a029562bd3b421ab8e3
zepumph <michael.kauzmann@colorado.edu>
2022-07-27 14:11:53 -0600

    add phetioDesigned: true, https://github.com/phetsims/geometric-optics/issues/438

--- js/gravity-force-lab-main.js
+++ js/gravity-force-lab-main.js
@@ -32,7 +32,8 @@ const simOptions = {
     soundDesign: 'Ashton Morris',
     thanks: 'Thanks to Mobile Learner Labs for working with the PhET development team to convert this simulation to HTML5.'
   },
-  hasKeyboardHelpContent: true
+  hasKeyboardHelpContent: true,
+  phetioDesigned: true
 };
 
 simLauncher.launch( () => {
