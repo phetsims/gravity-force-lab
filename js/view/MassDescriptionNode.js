a4e3e1b6a247e2e8481e7b2b3e68b35c692ceabc
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-01-05 12:22:50 -0700

    add author annotations (lint fix), https://github.com/phetsims/chipper/issues/1414

--- js/view/MassDescriptionNode.js
+++ js/view/MassDescriptionNode.js
@@ -1,9 +1,10 @@
 // Copyright 2018-2023, University of Colorado Boulder
+// @author Michael Kauzmann (PhET Interactive Simulations)
 
 
 import Multilink from '../../../axon/js/Multilink.js';
-import ISLCObjectDescriptionNode from '../../../inverse-square-law-common/js/view/ISLCObjectDescriptionNode.js';
 import ISLCObjectEnum from '../../../inverse-square-law-common/js/model/ISLCObjectEnum.js';
+import ISLCObjectDescriptionNode from '../../../inverse-square-law-common/js/view/ISLCObjectDescriptionNode.js';
 import merge from '../../../phet-core/js/merge.js';
 import StringUtils from '../../../phetcommon/js/util/StringUtils.js';
 import { Node } from '../../../scenery/js/imports.js';
