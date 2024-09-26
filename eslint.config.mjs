1abbb0dc25bfbf87fc74aa62174f7b9d95f21291
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:47 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for gravity-force-lab.
+ * ESLint configuration for gravity-force-lab.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
