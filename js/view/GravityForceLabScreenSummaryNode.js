7729bac5fc923c470706ed80d39ff58fd723c80c
phet-dev <phetadmin@gmail.com>
2024-10-28 03:40:29 -0600

    update copyright dates from daily grunt work

--- js/view/GravityForceLabScreenSummaryNode.js
+++ js/view/GravityForceLabScreenSummaryNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2024, University of Colorado Boulder
 
 /**
  * A node to control the PDOM content for the Screen Summary of the sim.
