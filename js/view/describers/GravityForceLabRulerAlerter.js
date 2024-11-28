26b30058856ce8917af047af11dbe3a094672692
phet-dev <phetadmin@gmail.com>
2024-11-28 03:35:27 -0700

    update copyright dates from daily grunt work

--- js/view/describers/GravityForceLabRulerAlerter.js
+++ js/view/describers/GravityForceLabRulerAlerter.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2023, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * This describer is responsible for the descriptions associated with the movable ruler. Unlike other describers, this
